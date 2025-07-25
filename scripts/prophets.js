eb0863cd524ae7c243e4aba7b6ae3278a6306b79
Ririku Shirakawa <ririkuchan@gmail.com>
2025-07-25 15:27:16 +0900

    W02 done

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = "https://byui-cse.github.io/cse121b/week03/data.json";
+const cards = document.querySelector(".cards");
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  displayProphets(data.prophets);
+}
+
+function displayProphets(prophets) {
+  prophets.forEach((prophet) => {
+    const card = document.createElement("section");
+    card.classList.add("card");
+
+    const name = document.createElement("h2");
+    name.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    const dob = document.createElement("p");
+    dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+
+    const pob = document.createElement("p");
+    pob.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+    const image = document.createElement("img");
+    image.src = prophet.imageurl;
+    image.alt = `Portrait of ${prophet.name} ${prophet.lastname}`;
+    image.loading = "lazy";
+
+    card.appendChild(name);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(image);
+
+    cards.appendChild(card);
+  });
+}
+
+getProphetData();
